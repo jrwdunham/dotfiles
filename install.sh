70bbc7bdef067ff92aa5dad0208746757c1d56b4
Joel Dunham <jrwdunham@gmail.com>
2015-12-29 15:07:45 -0800

    Added git aliases.

--- install.sh
+++ install.sh
@@ -3,3 +3,11 @@ curl -Sso ${HOME}/.vim/autoload/pathogen.vim https://raw.githubusercontent.com/t
 ln -sf ${PWD}/.vimrc ${HOME}/.vimrc
 ln -sf ${PWD}/.inputrc ${HOME}/.inputrc
 
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.br branch
+git config --global alias.hist "log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short"
+git config --global alias.type "cat-file -t"
+git config --global alias.dump "cate-file -p"
+
